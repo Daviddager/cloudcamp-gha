92765c2e33df4593fea69a04c85f3771ce9d4468
Daviddager <drdagerm@gmail.com>
2026-04-09 19:55:45 -0500

    :bricks: Fix bucket rules

--- main.tf
+++ main.tf
@@ -20,19 +20,6 @@ resource "aws_kms_key" "mykey" {
 resource "aws_s3_bucket" "example" {
   bucket = "921292479940-daviddager"
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-    rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "AES256"
-    }
-  }
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
